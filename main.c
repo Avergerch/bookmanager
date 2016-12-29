bd16540057528e9d28750f769cefdc37c9f1f6d0
Avergerch <avergerch@gmail.com>
2016-12-29 14:52:02 +0800

    Parted file_read into read and list, realized add and write.

--- main.c
+++ main.c
@@ -4,12 +4,12 @@
 
 
 //struct of book
-typedef struct s_book {
+typedef struct str_book {
         int count;
         char name[40];
         char auther[40];
         char type[40];
-} s_book;
+} str_book;
 
 
 //delete useless 'n' in the last of strings
@@ -19,19 +19,20 @@ void rm_n (char *string) {
 }
 
 
-//how to use:file_functions("file pointer","struct")
-void file_read(FILE *fp,s_book *book) {
+//function to read file. return the number of books.
+int file_read(FILE *fp,str_book *book) {
         char buf[130];
         int type1,type2;
         int count = 0;
         int i;
-        //狗屎！C不允许初始化以外字符串赋值给数组
+        //狗屎！C不允许初始化以外的字符串赋值给数组
         //并且不知道为什么结构体成员指针莫名爆炸
         //只能这样了
         char *name = (char*)malloc(sizeof(char) * 40);
         char *auther = (char*)malloc(sizeof(char) * 40);
         char *type = (char*)malloc(sizeof(char) * 40);
 
+        //count will equals number of books.
         while (fgets (buf,130,fp) && buf[0] != EOF) {
                 rm_n (buf);
 
@@ -48,17 +49,55 @@ void file_read(FILE *fp,s_book *book) {
         }
         printf ("Read %u books\n",count);
 
+        return count;
+}
+
+
+//function to list books
+void file_list (int count,str_book *book) {
+        int i;
+        printf ("Here's book's listi:\n");
         for (i = 0;i < count;i++)
-                printf ("%d. <%s> by %s , Type:%s\n"
-                ,i+1,book[i].name,book[i].auther,book[i].type);
+                printf ("%d. <%s> by %s, Type:%s\n"
+                        ,i+1,book[i].name,book[i].auther,book[i].type);
+}
+
+
+//function to add book
+int file_add (FILE *fp,str_book *book,int count) {
+        char buf[130];
+
+        if (count >= 100) {
+                puts("Error!List was full!");
+                return count;
+        }
+
+        printf ("Here's No.%d book to add\nName:",count+1);
+        fgets (book[count].name,40,stdin);
+        rm_n (book[count].name);
+        printf ("Auther:");
+        fgets (book[count].auther,40,stdin);
+        rm_n (book[count].auther);
+        printf ("Type:");
+        fgets (book[count].type,40,stdin);
+        rm_n (book[count].type);
+
+        book[count].count = count + 1;
+        
+        sprintf (buf,"%s-%s-%s\n"
+                 ,book[count].name,book[count].auther,book[count].type);
+        fputs (buf,fp);
+
+        return ++count;
 }
 
 
 //main function
 int main (int argc,char *argv[]) {
-        s_book book[100];
+        str_book book[100];
         FILE *fp;
         char newarg;
+        int count;
 
         if (argc != 2) {
                 puts ("Error!No or too much arguments");
@@ -69,23 +108,32 @@ int main (int argc,char *argv[]) {
                 return 1;
         }
 
-        printf ("######The Books Manager#######\n"
-                "r:read a:add d:delete q:quit\n"
-                "##############################\n"
-                ">");
-        scanf ("%c",&newarg);
+        printf ("#########The Books Manager#########\n"
+                "r:read l:list a:add d:delete q:quit\n"
+                "###################################\n");
 
-        //
-        switch (newarg) {
-                case 'r': file_read(fp,book); break;
-                /*
-                case 'a': file_add(); break;
-                case 'd': file_delete; break;
-                */
-                case 'q': return 0;
-                default: puts("Error!Arguments Exception!");
+        while (1) {
+                printf(">");
+                scanf ("%c",&newarg);
+                getchar();
+
+                switch (newarg) {
+                        case 'r': 
+                                count = file_read (fp,book);
+                                break;
+                        case 'l':
+                                file_list (count,book);
+                                break;
+                        case 'a': 
+                                count = file_add (fp,book,count);
+                                break;
+                        /*
+                        case 'd': file_delete; break;
+                        */
+                        case 'q': return 0;
+                        default: puts("Error!Arguments Exception!");
+                }
         }
 
-        fclose (fp);
         return 0;
 }
